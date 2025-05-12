adba51138a9e59fa1742ca7dfa301ee43463c905
arielperstin10 <arielperstin10@gmail.com>
2025-05-12 16:35:12 +0300

    pipleline test #4

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,4 +2,41 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { configure } from "@testing-library/react";
+
+// Mock the UserContext
+jest.mock("./context/UserContext", () => ({
+  useUser: () => ({
+    user: {
+      id: "1",
+      name: "Test User",
+      role: "student",
+    },
+    setUserData: jest.fn(),
+    userData: {
+      id: "1",
+      name: "Test User",
+      role: "student",
+    },
+  }),
+}));
+
+// Mock axios
+jest.mock("axios", () => ({
+  create: jest.fn(() => ({
+    get: jest.fn(),
+    post: jest.fn(),
+    put: jest.fn(),
+    delete: jest.fn(),
+  })),
+  get: jest.fn(),
+  post: jest.fn(),
+  put: jest.fn(),
+  delete: jest.fn(),
+}));
+
+// Configure testing library
+configure({
+  testIdAttribute: "data-testid",
+});
