c0b905f876e12b7aa4c020d7b063bbaa25dc8d48
udivak <udivak9@gmail.com>
2025-05-18 14:25:00 +0300

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Starting FastAPI Backend..."
-cd backend
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload &
+uvicorn backend.main:app --host 0.0.0.0 --port 8000 --reload &
 
 echo "Starting React Frontend..."
 cd ../frontend
